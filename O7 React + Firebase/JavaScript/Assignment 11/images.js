07b84613f1f556e923ca1678a4c3885d5233f921
Saara <saara.281106@gmail.com>
2026-06-16 13:46:55 +0530

    First Assignment of JavaScript

---
+++ O7 React + Firebase/JavaScript/Assignment 11/images.js
@@ -0,0 +1,12 @@
+function fun1(){
+    document.getElementById("a").src="pic1.jpeg"
+}
+function fun2(){
+    document.getElementById("a").src="pic2.jpg"
+}
+function fun3(){
+    document.getElementById("a").src="pic3.jpg"
+}
+function back(){
+    document.getElementById("a").src="mainpic.jpg"
+}
